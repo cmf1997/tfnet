d0aaa4593325ed55dddc8bb783b014c6093c39ac
cmf1997 <562590731@qq.com>
2023-11-14 10:48:07 +0800

    preprocess chip-seq

--- test.py
+++ test.py
@@ -11,8 +11,27 @@ blacklist_file = '/Users/cmf/Downloads/TFNet-multi-tf/data/tf_chip/filter_blackl
 blacklist = BedTool(blacklist_file)
 blacklist = blacklist.slop(g=genome_sizes_file, b=L)
 
-bedtools grep -i -header -f avai_chr.txt -fi blacklist.bed > filter_black.bed
 
 
 
 
+
+def get_genome_bed(genome_sizes_file):
+    genome_sizes_info = np.loadtxt(genome_sizes_file, dtype=str)
+    chroms = list(genome_sizes_info[:,0])
+    chroms_sizes = list(genome_sizes_info[:,1].astype(int))
+    genome_bed = []
+    for chrom, chrom_size in zip(chroms, chroms_sizes):
+        genome_bed.append(Interval(chrom, 0, chrom_size))
+    genome_bed = BedTool(genome_bed)
+    return chroms, chroms_sizes, genome_bed
+
+
+def get_genome_windows(genome_sizes_file, genome_window_size, genome_window_step):
+    genome_windows = BedTool().window_maker(g=genome_sizes_file, w=genome_window_size,s=genome_window_step)
+    return genome_windows
+
+bedtools makewindows -g /Users/cmf/Downloads/TFNet-multi-tf/data/tf_chip/hg19.chrom.sizes.reduced -w 1024 -s 100 > genome_windows.bed
+bedtools intersect -a genome_windows.bed -b GM12878_BATF.bed -wa -wb > annotated_windows.bed
+
+list.extend()
